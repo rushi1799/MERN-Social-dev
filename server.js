36506f7a3d757b32aba9d795f463c0d50e6e53bb
unknown <rushikeshbahekar17@gmail.com>
2020-09-10 02:13:27 +0530

    add initial routes and establish database connection

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+
+//Including the routes
+const usersRoutes = require("./routes/api/users");
+const profileRoutes = require("./routes/api/profile");
+const postsRoutes = require("./routes/api/posts");
+const authRoutes = require("./routes/api/auth");
+
+// Connect to Database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is working");
 });
 
+// Define Routes
+app.use("/api/users", usersRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/auth", authRoutes);
+
+// Sever config
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (err) => {
