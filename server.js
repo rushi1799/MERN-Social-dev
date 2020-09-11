3ee3d68354189e2cf9b47e4aa4d889fb7c7d646e
unknown <rushikeshbahekar17@gmail.com>
2020-09-11 23:55:34 +0530

    add react client directory to project

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/posts", postsRoutes);
 app.use("/api/auth", authRoutes);
 
 // Sever config
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, (err) => {
   if (err) {
