4566a1468989b1382aaf61e204f85e7952742e3b
unknown <rushikeshbahekar17@gmail.com>
2020-09-12 22:20:03 +0530

    add loaduser method to reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
 import NavBar from "./components/layouts/NavBar";
 import Landing from "./components/layouts/Landing";
 import Alert from "./components/layouts/Alert";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import setAuthToken from "./utils/setAuthToken";
 
 import "./App.css";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <Fragment>
-      <NavBar />
-      <section className="container">
-        <Alert />
-      </section>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <NavBar />
+          <section className="container">
+            <Alert />
+          </section>
 
-      <Route exact path="/" component={Landing} />
+          <Route exact path="/" component={Landing} />
 
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Fragment>
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
 
+//Register user
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
@@ -23,5 +30,18 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
     }
     dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+// load user
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({ type: USER_LOADED, payload: res.data });
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR });
   }
 };
